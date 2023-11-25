48339312693b346133f06e14d8924fb2d92396a1
zaki afandi <zakiafnd28@gmail.com>
2023-11-25 23:04:42 +0700

    up

---
+++ binnet/app/Models/data_user_binnet.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class data_user_binnet extends Model
+{
+    use HasFactory;
+}
